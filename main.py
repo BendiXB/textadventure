988638c518568d9480e5b4a2588b685dcf6eb203
ocka <oka02@gmx.de>
2020-06-05 12:56:23 +0200

    erstellung der Grundstruktur

---
+++ main.py
@@ -0,0 +1,29 @@
+class spiel():
+    def __init__(self):
+        self.eingabe = 0
+    def start(self):
+        self.eingabe= input("Was willst du?")
+        print(self.eingabe)
+
+class welt():
+    def __init__(self):
+        #etwas
+
+
+class wesen():
+    def __init__(self):
+        #etwas
+
+class spieler():        #erbt von wesen
+    def __init__(self):
+        #etwas
+
+class monster():        # erbt von wesen
+    def __init__(self):
+        #etwas
+
+
+
+
+
+
