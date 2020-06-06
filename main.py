8ea8011a51ca5d9c9428ad0ec31ec5428fb65bd3
ocka <oka02@gmx.de>
2020-06-06 15:20:03 +0200

    Erweiterung der Classen durch Attribute und Behebung einer falsch deklarierten Klasse

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 class spiel():
     def __init__(self):
         self.eingabe = 0
+        self.version = 0.1
+        self.credits= ("Bendix und Oscar")
     def start(self):
         self.eingabe= input("Was willst du?")
         print(self.eingabe)
+    #def shop(self):       #noch in arbeit
+
 
 class welt():
-    def __init__(self):
-        #etwas
-
+    def __init__(self, lootmult, monstermult, gegnerbisloot ):
+        self.lootmult = lootmult
+        self.monstermult = monstermult
+        self.gegnerbisloot = gegnerbisloot
+    #def rndgegner(self):
 
 class wesen():
-    def __init__(self):
-        #etwas
+    def __init__(self, name, leben, schaden, asciiart):
+        self.name = name
+        self.leben = leben
+        self.schaden = schaden
+        self.asciiart = asciiart
 
+
+
+
+"""
 class spieler():        #erbt von wesen
     def __init__(self):
         #etwas
@@ -22,7 +35,7 @@ class monster():        # erbt von wesen
     def __init__(self):
         #etwas
 
-
+"""
 
 
 
