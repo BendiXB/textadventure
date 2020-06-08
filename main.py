9bc91964da9a3a59eb09fcb87220654adfc336cb
ocka <oka02@gmx.de>
2020-06-08 10:42:30 +0200

    verbesserung der kampfmechanik

--- main.py
+++ main.py
@@ -65,10 +65,10 @@ class welt():
             nextgegner=Monster1
         elif x == 5:
             nextgegner=Monster1
-        print("Du triffst einen ", nextgegner.name, "\n Willst du wegrennen[1] oder ihn angreifen[2]\n")
+        print("Du triffst einen ", nextgegner.name, "\nWillst du wegrennen[1] oder ihn angreifen[2]\n")
         i = input()
         if i == "1":
-            x = randint(1,2)
+            x = randint(1,3)
             if x == 1:
                 print("du kannst wegrennen!! glück gehabt!!")
             else:
@@ -94,11 +94,24 @@ class wesen():
             gegner.leben= gegner.leben - self.schaden
             if self.leben >0 and gegner.leben >0:
                 self.leben=self.leben-gegner.schaden
-            print(self.leben,gegner.leben)
+            print(self.leben,"Leben von:",self.name,"    ",gegner.leben,"Leben von:",gegner.name)
+
+
         if self.leben >0:
             print(self.name, " hat gewonnen #angreifen func")
+            if isinstance(self, spieler):
+                gegner.sterben()
+            if isinstance(self, monster):
+                self.sterben()
+                gegner.sterben()
+
         else:
             print(gegner.name,"hat gewonnen #angreifen func")
+            if isinstance(gegner, monster):
+                self.sterben()
+                gegner.sterben()
+
+
 
 class spieler(wesen):
     def __init__(self, name, asciiart):
@@ -109,15 +122,22 @@ class spieler(wesen):
         super().__init__( name,  asciiart)
     def sterben(self):                      #nach dem tot bekommt der Spieler eine 2te chance aber ist 20% schwächer
         self.leben = 20
-        self.maxlaben = self.maxlaben*0.8
+        self.maxleben = self.maxleben*0.8
         self.schaden = self.schaden*0.8
-        print("du bist gestorben")
+        print("\n\n\ndu bist gestorben deine eingeweide werden von einem hobby alchemisten aufgelesen\nund mit einer schnecke gekreuzt. Dadruch lebst du wieder, verlierst aber 20% deiner staerke!!\n\n\n")
+
+
+
 class monster(wesen):
     def __init__(self, name, asciiart, leben, schaden):
         self.leben = leben
+        self.maxleben = leben
         self.schaden = schaden
         super().__init__(name,asciiart)
-    #def gestorben(self):
+    def sterben(self):
+        self.leben=self.maxleben
+
+
 
 asciispieler="x["
 
@@ -134,16 +154,15 @@ class spiel():
         while self.lustvomspieler == 1:
             self.eingabe = input("Willst du die Welt[1]? erkunden oder in den Dorfladen[2] gehen?\n")
             if self.eingabe == "1":
-              print("wilkommen in der welt")
               World1.erkunden(LocalSpieler)
             else:
                 print("Du bist im Laden")
 
 
 a="xD"
-Monster1 = monster("Wolf", a, 20, 1)
+Monster1 = monster("Wolf", a, 20, 15)
 World1= welt("Wald",1,1,1)
-
+#isinstance()
 
 Session1 = spiel()
 Session1.start()
